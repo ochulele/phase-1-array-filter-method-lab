6d46716bfc40fc4d62a4fe65f9d7306cd0e6e3ff
ochuk <ochuciano@gmail.com>
2023-04-16 11:10:05 -0400

    array filter method

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution here
+function findMatching(arrays, string) {
+    return arrays.filter(function (array) { return array.toLowerCase() === string.toLowerCase() })
+}
+
+function fuzzyMatch(arrays, string) {
+    return arrays.filter(function (array) { return array.toLowerCase().substring(0, string.length) === string.toLowerCase() })
+}
+
+function matchName(arrays, string) {
+    return arrays.filter( function (array) { return array.name === string})
+}
